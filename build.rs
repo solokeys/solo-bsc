1451cf3d6f0048bb05f89090cc751477e89b8efd
Nicolas Stalder <n@stalder.io>
2019-03-12 02:23:01 +0100

    Final commit to yank, renamed to solo-bsc

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-env-changed=BOARD");
-    println!("cargo:rerun-if-changed=memory.x");
-    println!("cargo:rerun-if-changed=build.rs");
-}
-
--- openocd.gdb
+++
@@ -1,3 +0,0 @@
-target remote :3333
-set print asm-demangle on
-monitor reset halt
